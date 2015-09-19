f41bc736adcd0d74f93386892e13a0d2567a3d94
Maxim <maximum454@gmail.com>
2015-09-19 09:57:01 +0600

    page
    season

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(function(){
 
 	$('.bx-slider').bxSlider();
 
+	$("ul.tabs").tabs("div.panes > div");
+
 	$('.popap_box').click(function() {
 		$('#modalbox_' + $(this).data('body')).arcticmodal();
 	});
