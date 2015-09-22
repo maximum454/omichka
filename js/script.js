01ea96dbcaf1e91eca62953c065a2bd9c2fe9e3d
Maxim <maximum454@gmail.com>
2015-09-22 22:04:14 +0600

    table
    Турнирная таблица

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@ $(function(){
 
 	$('.bx-slider').bxSlider();
 
-	$("ul.tabs").tabs("div.panes > div");
+	bxarena =$('.bxarena').bxSlider({
+		auto:0,
+		pager:1,
+		controls:1,
+		nextSelector: '.next-arena',
+  		prevSelector: '.pre-arena',
+  		pagerSelector:'.b-count'
+	});
+	slideQty = bxarena.getSlideCount();
+
+	$(".tabs").lightTabs();
 
 	$('.popap_box').click(function() {
 		$('#modalbox_' + $(this).data('body')).arcticmodal();
